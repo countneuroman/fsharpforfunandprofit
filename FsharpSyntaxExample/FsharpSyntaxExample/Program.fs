dd200bbf7d4b963da3779af1162e2e55260ac76e
CountNeuroman <olorinoren@gmail.com>
2022-09-18 20:25:52 +0300

    Add syntax example app

---
+++ FsharpSyntaxExample/FsharpSyntaxExample/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+//Define named function
+let addTwoNumbers x y = x + y
+
+Console.WriteLine(addTwoNumbers 2 3)
+
+
+let oneToTen = [1..10]
+let evens list =
+    let isEven x =x % 2 = 0
+    List.filter isEven list
+    
+evens oneToTen |> Seq.iter (printf "%d ")
